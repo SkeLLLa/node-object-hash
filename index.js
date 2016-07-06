85d8232af6f8c4249924e9ab8c15cb6c52b0d54e
Oleksandr Kureniov <oleksandr.krureniov@betlab.com>
2016-07-06 12:43:40 +0300

    initial commit

--- index.js
+++ index.js
@@ -1,5 +1,79 @@
 /**
- * Created on 7/5/16.
+ * Node.js constant object hash module.
+ * @exports node-object-hash
+ * @type {Function}
  */
+
 'use strict';
+const crypto = require('crypto');
+
+/**
+ * Sorts object fields
+ * @param {Object|Array|string|function} obj Initial object
+ * @param {boolean} [coerce=true] Perform coercion
+ * @param {boolean} [sort=true] Perform Array, Set, Object sorting
+ * @returns {string}
+ */
+exports.sortedObjectString = (obj, {coerce = true, sort = true}) => {
+  if (typeof obj == 'object') {
+    if (Array.isArray(obj)) {
+      let tmp = [...obj];
+      tmp.forEach((it, idx) => {
+        tmp[idx] = exports.sortedObjectString(it, {coerce, sort})
+      });
+      return sort ? `[${tmp.sort().toString()}]` : `[${tmp.toString()}]`;
+    }
+    if (obj === null) {
+      return `null`;
+    }
+    if (obj instanceof Map) {
+      return `Map[${[...obj].toString()}]`;
+    }
+    if (obj instanceof Set) {
+      return sort ? `Set[${[...obj].sort().toString()}]` : `Set[${[...obj].toString()}]`;
+    }
+    const sortedObj = new Map();
+    const keys = sort ? Object.keys(obj).sort() : Object.keys(obj);
+    keys.forEach((key) => {
+      sortedObj.set(key, exports.sortedObjectString(obj[key], {coerce, sort}));
+    });
+    return `{${[...sortedObj].toString()}}`;
+  }
+  if (typeof obj == 'function') {
+    return `${obj.name}=>${obj.toString()}`;
+  }
+  if (coerce) {
+    if (typeof obj == 'boolean') {
+      return `${Number(obj)}`;
+    }
+  } else {
+    if (typeof obj == 'string') {
+      return `"${obj}"`
+    }
+  }
+  if (coerce && typeof obj == 'boolean') {
+    return `${Number(obj)}`;
+  }
+  return obj;
+};
+
+/**
+ * Calculates object hash
+ * @param {Object} obj Object to hash
+ * @param {string} [alg="sha256"] Crypto algorithm to use
+ * @param {string} [enc="hex"] Hash string encoding
+ * @param {boolean} [coerce=true] Perform coercion
+ * @param {boolean} [sort=true] Perform Array, Set, Object sorting
+ * @returns {string} Hash string
+ */
+exports.hash = (obj, {alg='sha256', enc='hex', coerce=true, sort=true} = {}) => {
+  if (~crypto.getHashes().indexOf(alg)) {
+    const sorted = exports.sortedObjectString(obj, {coerce, sort});
+    return crypto.createHash(alg)
+      .update(sorted)
+      .digest(enc);
+  } else {
+    throw new Error(`Algorithm ${alg} not supported by "ctypto" module`);
+  }
+};
 
--- test/sortedObjectString.js
+++ test/sortedObjectString.js
@@ -3,3 +3,55 @@
  */
 'use strict';
 
+const sortedObjectString = require('../index').sortedObjectString;
+const assert = require('chai').assert;
+
+const object = {
+  x: new Map([['a', 1], ['c', 3], ['b', 2]]),
+  z: new Set([4,3,2,1]),
+  f: 2,
+  e: [{a:2}, 3, {g:2, e:{d:1}}, "rt", "1", "2abc", "3d"],
+  d: [3,5,6,1,2,3],
+  c: "3ab",
+  b: "2",
+  a: 1
+};
+
+describe(`node-object-hash`, () => {
+  describe(`#sortedObjectString(obj, {coerce: true, sort: true})`, () => {
+    it(`should return properly sorted object string`, () => {
+      let options = {coerce: true, sort: true};
+      assert.equal(sortedObjectString(object, options), `{a,1,b,2,c,3ab,d,[1,2,3,3,5,6],e,[1,2abc,3,3d,rt,{a,2},{e,{d,1},g,2}],f,2,x,Map[a,1,c,3,b,2],z,Set[1,2,3,4]}`);
+    });
+  });
+});
+
+
+describe(`node-object-hash`, () => {
+  describe(`#sortedObjectString(obj, {coerce: true, sort: false})`, () => {
+    it(`should return properly sorted object string`, () => {
+      let options = {coerce: true, sort: false};
+      assert.equal(sortedObjectString(object, options), `{x,Map[a,1,c,3,b,2],z,Set[4,3,2,1],f,2,e,[{a,2},3,{g,2,e,{d,1}},rt,1,2abc,3d],d,[3,5,6,1,2,3],c,3ab,b,2,a,1}`);
+    });
+  });
+});
+
+
+describe(`node-object-hash`, () => {
+  describe(`#sortedObjectString(obj, {coerce: false, sort: true})`, () => {
+    it(`should return properly sorted object string`, () => {
+      let options = {coerce: false, sort: true};
+      assert.equal(sortedObjectString(object, options), `{a,1,b,"2",c,"3ab",d,[1,2,3,3,5,6],e,["1","2abc","3d","rt",3,{a,2},{e,{d,1},g,2}],f,2,x,Map[a,1,c,3,b,2],z,Set[1,2,3,4]}`);
+    });
+  });
+});
+
+
+describe(`node-object-hash`, () => {
+  describe(`#sortedObjectString(obj, {coerce: false, sort: false})`, () => {
+    it(`should return properly sorted object string`, () => {
+      let options = {coerce: false, sort: false};
+      assert.equal(sortedObjectString(object, options), `{x,Map[a,1,c,3,b,2],z,Set[4,3,2,1],f,2,e,[{a,2},3,{g,2,e,{d,1}},"rt","1","2abc","3d"],d,[3,5,6,1,2,3],c,"3ab",b,"2",a,1}`);
+    });
+  });
+});
